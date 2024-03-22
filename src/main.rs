ed87adb0ac7fbe27622759f694b9246b662c3cae
xfnw <xfnw@riseup.net>
2024-03-22 19:54:06 -0400

    load files into ram

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use git2::Repository;
-use std::path::PathBuf;
+use std::{collections::BTreeMap, path::PathBuf};
 
 #[derive(Debug, Parser)]
 struct Opt {
@@ -11,18 +11,45 @@ struct Opt {
     branch: String,
 }
 
+fn walk_callback(
+    repo: &Repository,
+    dir: &str,
+    entry: &git2::TreeEntry,
+    dir_map: &mut BTreeMap<String, Vec<(String, Vec<u8>)>>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let object = entry.to_object(repo)?;
+    let name = entry.name().ok_or("invalid unicode in a file name")?;
+
+    let blob = match object.into_blob() {
+        Ok(blob) => blob,
+
+        Err(_) => {
+            // is probably a directory
+            dir_map.insert(format!("{}{}/", dir, name), vec![]);
+            return Ok(());
+        }
+    };
+
+    let directory = dir_map.get_mut(dir).ok_or("VERBODEN TOEGANG")?;
+    directory.push((name.to_string(), blob.content().to_vec()));
+
+    Ok(())
+}
+
 fn main() {
     let opt = Opt::parse();
 
-    let repo = Repository::open(opt.repository).unwrap();
+    let repo = Repository::open(&opt.repository).unwrap();
     let commit = repo.revparse_single(&opt.branch).unwrap();
     let id = commit.short_id().unwrap();
     let id = id.as_str().unwrap();
     let commit = commit.into_commit().unwrap();
     let tree = commit.tree().unwrap();
+    let mut dir_map = BTreeMap::new();
+    dir_map.insert("".to_string(), vec![]);
 
-    tree.walk(git2::TreeWalkMode::PostOrder, move |dir, entry| {
-        dbg!(&id, dir, entry.name().unwrap());
+    tree.walk(git2::TreeWalkMode::PreOrder, |dir, entry| {
+        walk_callback(&repo, dir, entry, &mut dir_map).unwrap();
         0
     })
     .unwrap();
