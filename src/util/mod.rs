8161a57b2e6809a3d926fba101e4dd37661f29c0
xfnw <xfnw@riseup.net>
2024-08-16 11:41:54 -0400

    util: share functions more

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,3 +1,52 @@
+use git2::Blob;
+use orgize::{ast::Link, Org};
+use rowan::ast::AstNode;
+use std::{
+    collections::HashSet,
+    path::{Component, Path, PathBuf},
+};
+
 pub mod jsonindex;
 pub mod orphan;
 pub mod preview;
+
+pub fn find_links(name: &Path, blob: Blob, links: &mut HashSet<PathBuf>) {
+    let fstr = std::str::from_utf8(blob.content()).unwrap();
+    let res = Org::parse(fstr);
+    let document = res.document();
+    let syntax = document.syntax();
+    for descendant in syntax.descendants() {
+        let Some(link) = Link::cast(descendant) else {
+            continue;
+        };
+        let path = link.path();
+        let path = match path.split_once('#') {
+            Some((p, _)) => p,
+            None => &path,
+        };
+        let parent = name.parent().expect("borked name");
+        let fullpath = parent.join(path);
+        let fullpath = normalize(&fullpath);
+        links.insert(fullpath);
+    }
+}
+
+// why is this not a thing in the std???
+// https://github.com/rust-lang/rfcs/issues/2208
+pub fn normalize(path: &Path) -> PathBuf {
+    let mut res = PathBuf::new();
+
+    for component in path.components() {
+        match component {
+            Component::Prefix(_) => panic!("no windows"),
+            Component::RootDir => res.push("/"),
+            Component::CurDir => (),
+            Component::ParentDir => {
+                res.pop();
+            }
+            Component::Normal(n) => res.push(n),
+        }
+    }
+
+    res
+}
--- src/util/orphan.rs
+++ src/util/orphan.rs
@@ -1,10 +1,6 @@
-use git2::{Blob, Object, Repository};
-use orgize::{ast::Link, Org};
-use rowan::ast::AstNode;
-use std::{
-    collections::HashSet,
-    path::{Component, Path, PathBuf},
-};
+use super::find_links;
+use git2::{Object, Repository};
+use std::{collections::HashSet, path::PathBuf};
 
 pub fn get_orphans(repo: &Repository, commit: Object) -> HashSet<PathBuf> {
     let commit = commit.into_commit().unwrap();
@@ -34,45 +30,4 @@ pub fn get_orphans(repo: &Repository, commit: Object) -> HashSet<PathBuf> {
     .unwrap();
 
     pages.difference(&links).cloned().collect()
-}
-
-fn find_links(name: &Path, blob: Blob, links: &mut HashSet<PathBuf>) {
-    let fstr = std::str::from_utf8(blob.content()).unwrap();
-    let res = Org::parse(fstr);
-    let document = res.document();
-    let syntax = document.syntax();
-    for descendant in syntax.descendants() {
-        let Some(link) = Link::cast(descendant) else {
-            continue;
-        };
-        let path = link.path();
-        let path = match path.split_once('#') {
-            Some((p, _)) => p,
-            None => &path,
-        };
-        let parent = name.parent().expect("borked name");
-        let fullpath = parent.join(path);
-        let fullpath = normalize(&fullpath);
-        links.insert(fullpath);
-    }
-}
-
-// why is this not a thing in the std???
-// https://github.com/rust-lang/rfcs/issues/2208
-fn normalize(path: &Path) -> PathBuf {
-    let mut res = PathBuf::new();
-
-    for component in path.components() {
-        match component {
-            Component::Prefix(_) => panic!("no windows"),
-            Component::RootDir => res.push("/"),
-            Component::CurDir => (),
-            Component::ParentDir => {
-                res.pop();
-            }
-            Component::Normal(n) => res.push(n),
-        }
-    }
-
-    res
 }
