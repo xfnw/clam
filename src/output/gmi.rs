719a52e67cf061419c5d98a3f0841dd236a1aae8
xfnw <xfnw@riseup.net>
2025-07-01 22:13:26 -0400

    gmi: adapt html's generate_page

--- src/output/gmi.rs
+++ src/output/gmi.rs
@@ -1,8 +1,8 @@
 use crate::{
     config::ClamConfig,
     git::HistMap,
-    helpers::URL_PATH_UNSAFE,
-    output::{PageMetadata, Pages},
+    helpers::{org_links, URL_PATH_UNSAFE},
+    output::{get_keywords, infer_title, PageMetadata, Pages},
     Error,
 };
 use orgize::{
@@ -12,6 +12,9 @@ use orgize::{
 use percent_encoding::utf8_percent_encode;
 use std::{
     collections::HashMap,
+    ffi::OsStr,
+    fs::File,
+    io::Write,
     path::{Path, PathBuf},
     rc::Rc,
 };
@@ -50,14 +53,45 @@ impl Traverser for GmiExport {
 }
 
 pub fn generate_page(
-    _dir: &str,
-    _name: &str,
-    _file: &[u8],
-    _org_cfg: &ParseConfig,
-    _pages: &mut Pages,
-    _links: &mut HashMap<PathBuf, Vec<Rc<PathBuf>>>,
+    dir: &str,
+    name: &str,
+    file: &[u8],
+    org_cfg: &ParseConfig,
+    pages: &mut Pages,
+    links: &mut HashMap<PathBuf, Vec<Rc<PathBuf>>>,
 ) -> Result<(), Error> {
-    todo!()
+    let mut full_path: PathBuf = format!("{dir}{name}").into();
+    if Some("org") == full_path.extension().and_then(OsStr::to_str) {
+        let fstr = std::str::from_utf8(file).map_err(Error::NonUTF8Org)?;
+        let res = org_cfg.clone().parse(fstr);
+
+        let title = res.title().unwrap_or_else(|| infer_title(&full_path));
+
+        let old_path = full_path.clone();
+        full_path.set_extension("gmi");
+
+        let mypath = Rc::new(full_path.clone());
+        org_links(&res, &full_path, |mut l| {
+            l.set_extension("gmi");
+
+            if let Some(e) = links.get_mut(&l) {
+                e.push(mypath.clone());
+            } else {
+                links.insert(l, vec![mypath.clone()]);
+            }
+        });
+
+        let keywords = get_keywords(&res);
+        let mut gmi_export = GmiExport::default();
+        res.traverse(&mut gmi_export);
+        let gmi = gmi_export.finish();
+
+        pages.insert(full_path, (title, old_path, keywords, gmi));
+    } else {
+        let mut f = File::create(full_path).map_err(Error::File)?;
+        f.write_all(file).map_err(Error::File)?;
+    }
+    Ok(())
 }
 
 pub fn write_org_page(
