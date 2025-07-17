c795d3192e8ff7b4c923f98020fb311031f4b8aa
xfnw <xfnw@riseup.net>
2025-07-17 12:43:54 -0400

    use an actual url parser for listing links
    conveniently url is already a dependency of git2

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,6 +1,7 @@
 use orgize::{ast::Link, rowan::ast::AstNode, Org};
 use percent_encoding::{AsciiSet, CONTROLS};
-use std::path::{Component, Path, PathBuf};
+use std::path::{Path, PathBuf};
+use url::Url;
 
 pub const URL_UNSAFE: &AsciiSet = &CONTROLS
     .add(b' ')
@@ -17,60 +18,46 @@ pub const URL_UNSAFE: &AsciiSet = &CONTROLS
 
 pub const URL_PATH_UNSAFE: &AsciiSet = &URL_UNSAFE.add(b'#').add(b'?');
 
-/// run a function on every link in a syntax tree
+/// run a function on every link in a syntax tree, as a [`PathBuf`]
 ///
-/// will give mangled paths when encountering links to
-/// external resources.
+/// will skip links to external resources, and adds `index.org` to links to directories, use
+/// [`org_urls`] if you do not want that
 pub fn org_links<F>(res: &Org, name: &Path, mut callback: F)
 where
     F: FnMut(PathBuf),
 {
+    org_urls(res, name, |url| {
+        if url.scheme() != "file" {
+            return;
+        }
+        let Ok(fullpath) = url.to_file_path() else {
+            return;
+        };
+        // wasteful :(
+        let mut fullpath = fullpath.strip_prefix("/").unwrap().to_owned();
+        if url.path().ends_with('/') {
+            fullpath.push("index.org");
+        }
+        callback(fullpath);
+    });
+}
+
+/// run a function on every link in a syntax tree, as a [`Url`]
+pub fn org_urls<F>(res: &Org, name: &Path, mut callback: F)
+where
+    F: FnMut(Url),
+{
+    let fileroot = Url::from_file_path(Path::new("/").join(name))
+        .expect("current path should fit in a file url");
     let document = res.document();
     let syntax = document.syntax();
     for descendant in syntax.descendants() {
         let Some(link) = Link::cast(descendant) else {
             continue;
         };
-        let path = link.path();
-        let path = path.strip_prefix("file:").unwrap_or(&path);
-        let path = match path.split_once('#') {
-            Some((p, _)) => p,
-            None => path,
+        let Ok(url) = fileroot.join(&link.path()) else {
+            continue;
         };
-        let parent = name.parent().expect("borked name");
-        let fullpath = parent.join(path);
-        let mut fullpath = normalize(&fullpath);
-        match fullpath.extension().map(|e| e == "org") {
-            Some(true) => (),
-            _ => fullpath.push("index.org"),
-        }
-        callback(fullpath);
+        callback(url);
     }
-}
-
-/// normalize a path *without* checking actual files.
-///
-/// this may give incorrect answers when symlinks are
-/// involved. use [`std::fs::canonicalize`] instead
-/// when this is an issue.
-///
-/// panics if given a windows-style path prefix.
-// why is this not a thing in the std???
-// https://github.com/rust-lang/rfcs/issues/2208
-pub fn normalize(path: &Path) -> PathBuf {
-    let mut res = PathBuf::new();
-
-    for component in path.components() {
-        match component {
-            Component::Prefix(_) => panic!("no windows"),
-            Component::RootDir => res.push("/"),
-            Component::CurDir => (),
-            Component::ParentDir => {
-                res.pop();
-            }
-            Component::Normal(n) => res.push(n),
-        }
-    }
-
-    res
 }
