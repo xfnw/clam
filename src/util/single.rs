b8484922c2644b2ed3209f9d436d0a6e750f25cd
xfnw <xfnw@riseup.net>
2025-08-01 13:21:56 -0400

    single: hook up output

--- src/util/single.rs
+++ src/util/single.rs
@@ -1,18 +1,23 @@
+use crate::{Error, output::infer_title};
 use git2::{Commit, Repository};
 use html_escaper::{Escape, Trusted};
-use orgize::export::{Container, Event, HtmlEscape, Traverser};
+use orgize::{
+    ParseConfig,
+    export::{Container, Event, HtmlEscape, Traverser},
+};
 use slugify::slugify;
+use std::{ffi::OsStr, path::PathBuf};
 use url::Url;
 
 #[derive(boilerplate::Boilerplate)]
 struct SingleHtml<'a> {
-    entries: &'a [&'a Entry<'a>],
+    entries: &'a [Entry],
 }
 
-struct Entry<'a> {
-    title: &'a str,
+struct Entry {
+    title: String,
     slug: String,
-    body: &'a str,
+    body: String,
 }
 
 #[derive(Default)]
@@ -67,6 +72,57 @@ fn slug_url(url: impl AsRef<str>) -> String {
     url.to_string()
 }
 
-pub fn print_html(_repo: &Repository, _commit: &Commit) {
-    todo!()
+fn generate_entry(
+    dir: &str,
+    name: &str,
+    file: &[u8],
+    org_cfg: &ParseConfig,
+    entries: &mut Vec<Entry>,
+) -> Result<(), Error> {
+    let full_path = format!("{dir}{name}");
+    let bpath = PathBuf::from(&full_path);
+    let (title, body) = if bpath
+        .extension()
+        .and_then(OsStr::to_str)
+        .is_some_and(|s| s.eq_ignore_ascii_case("org"))
+    {
+        let fstr = str::from_utf8(file).map_err(Error::NonUTF8Org)?;
+        let res = org_cfg.clone().parse(fstr);
+        let title = res.title().unwrap_or_else(|| infer_title(&bpath));
+
+        let mut html_export = LinkSlugExport::default();
+        res.traverse(&mut html_export);
+        let body = html_export.exp.exp.finish();
+
+        (title, body)
+    } else {
+        // TODO: include other kinds of files that are utf-8
+        return Ok(());
+    };
+
+    entries.push(Entry {
+        title,
+        slug: slugify!(&full_path),
+        body,
+    });
+
+    Ok(())
+}
+
+pub fn print_html(repo: &Repository, commit: &Commit) {
+    let tree = commit.tree().unwrap();
+    let mut entries = vec![];
+    let org_cfg = crate::default_org_cfg();
+
+    tree.walk(git2::TreeWalkMode::PreOrder, |dir, entry| {
+        if let Err(e) = crate::git::walk_callback(repo, dir, entry, |name, blob| {
+            generate_entry(dir, name, blob.content(), &org_cfg, &mut entries)
+        }) {
+            eprintln!("{e}");
+        }
+        0
+    })
+    .unwrap();
+
+    println!("{}", SingleHtml { entries: &entries });
 }
